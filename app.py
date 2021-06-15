da62953fdf0cbdc1fc88388c6c4bdcecea5c8334
Roton647 <abarroso@edu.uwaterloo.ca>
2021-06-15 09:54:45 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+
+@app.route('/api/time')
+def hello_world():
+    return {'time': time.time()}
+
+
+if __name__ == '__main__':
+    app.run()
